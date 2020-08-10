0a10ab853e72f52ebbc5792ed9eebc9009c039b8
Preeti Gupta <preetigupta510@gmail.com>
2020-08-09 19:05:56 -0700

    code for rendering the static photo as json

--- click-game/src/App.js
+++ click-game/src/App.js
@@ -1,8 +1,55 @@
-import React from "react";
-import SearchResultContainer from "./components/SearchResultContainer";
 
-function App() {
-  return <SearchResultContainer />;
+import React, { Component } from "react";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import Score from "./components/Score";
+import pups from "./cards.json";
+import "./App.css";
+// import API from "../utils/API";
+
+class App extends Component {
+  // Setting this.state.pups to the cards json array
+  state = {
+    pups,
+    clickedPuppyIds: [],
+    score: 0,
+    goal: 8,
+    status: ""
+  };
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">The Clickster</h1>
+          <p className="App-intro">
+            Try not to click the same image twice!
+          </p>
+        </header>
+        
+        <Score total={this.state.score}
+               goal={8}
+               status={this.state.status}
+               />
+        <Wrapper>
+          {this.state.pups.map(puppy => (
+            <Card
+              shuffleScoreCard={this.shuffleScoreCard}
+              id={puppy.id}
+              key={puppy.id}
+              image={puppy.image}
+            />
+          ))}
+        </Wrapper>
+        <footer>
+          <p>Designed and built by Heather Mathies. You can find the
+          code<a href="https://github.com/hmathies/Clicky-Game" target="_blank" rel="noopener noreferrer"> here</a>.</p>
+        </footer>
+    </div>
+    );
+  }
+//}
 }
 
 export default App;
---
+++ click-game/src/components/Card.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Card(props) {
+  return (
+    <div className="card text-center">
+      <div className="card-header">
+      <img alt={props.name} src={props.image} id={props.id}
+        onClick={() => props.shuffleScoreCard(props.id)} className='shuffleScore'/>
+      </div>
+      <div className="card-body">{props.children}</div>
+    </div>
+  );
+}
+
+export default Card;
+
+// import React from "react";
+// import "./Card.css";
+
+// //this component is only rendering the images to the screen and can be a simple render function it is a
+// //stateless component
+// const Card = props => (
+//   <div className="card img-container hover">
+//       <img alt={props.name} src={props.image} id={props.id}
+//         onClick={() => props.shuffleScoreCard(props.id)} className='shuffleScore'/>
+//   </div>
+// );
+
+// export default Card;
