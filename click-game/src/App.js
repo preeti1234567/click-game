15436c6272d20f877ba718b9c030e0e18750fef8
Preeti Gupta <preetigupta510@gmail.com>
2020-08-09 19:57:01 -0700

    adding style to the card

--- click-game/src/App.js
+++ click-game/src/App.js
@@ -5,7 +5,7 @@ import Wrapper from "./components/Wrapper";
 import Score from "./components/Score";
 import pups from "./cards.json";
 import "./App.css";
-// import API from "../utils/API";
+//import API from "../utils/API";
 
 class App extends Component {
   // Setting this.state.pups to the cards json array
@@ -17,7 +17,32 @@ class App extends Component {
     status: ""
   };
 
+//   //shuffle the pup cards in the browser when clicked
+//   shuffleScoreCard = id => {
+//     let clickedPuppyIds = this.state.clickedPuppyIds;
 
+//     if(clickedPuppyIds.includes(id)){
+//       this.setState({ clickedPuppyIds: [], score: 0, status:  "Game Over! You lost. Click to play again!" });
+//       return;
+//     }else{
+//       clickedPuppyIds.push(id)
+
+//       if(clickedPuppyIds.length === 8){
+//         this.setState({score: 8, status: "You Won! Great Job, Smartie! Click to play again!", clickedPuppyIds: []});
+//         console.log('You Win');
+//         return;
+//       }
+
+//       this.setState({ pups, clickedPuppyIds, score: clickedPuppyIds.length, status: " " });
+
+//       for (let i = pups.length - 1; i > 0; i--) {
+//         let j = Math.floor(Math.random() * (i + 1));
+//         [pups[i], pups[j]] = [pups[j], pups[i]];
+//       }
+//     }
+//   }
+
+//   // Map over this.state.cards and render a Card component for each card object
   render() {
     return (
       <div className="App">
@@ -42,10 +67,7 @@ class App extends Component {
             />
           ))}
         </Wrapper>
-        <footer>
-          <p>Designed and built by Heather Mathies. You can find the
-          code<a href="https://github.com/hmathies/Clicky-Game" target="_blank" rel="noopener noreferrer"> here</a>.</p>
-        </footer>
+        
     </div>
     );
   }
--- click-game/src/components/Card.js
+++ click-game/src/components/Card.js
@@ -1,29 +1,27 @@
 import React from "react";
+import"./Card.css";
+
+// function Card(props) {
+//   return (
+//     <div className="card text-center">
+//       <div className="card-header">
+//       <img alt={props.name} src={props.image} id={props.id}
+//         onClick={() => props.shuffleScoreCard(props.id)} className='shuffleScore'/>
+//       </div>
+//       <div className="card-body">{props.children}</div>
+//     </div>
+//   );
+// }
 
 function Card(props) {
-  return (
-    <div className="card text-center">
-      <div className="card-header">
-      <img alt={props.name} src={props.image} id={props.id}
-        onClick={() => props.shuffleScoreCard(props.id)} className='shuffleScore'/>
-      </div>
-      <div className="card-body">{props.children}</div>
-    </div>
-  );
-}
+    return (
+      <div class="col-md-4">
+        <img alt={props.name} src={props.image} id={props.id}
+          onClick={() => props.shuffleScoreCard(props.id)} className='shuffleScore'/>
+        </div>
+        
+    );
+  }
 
 export default Card;
 
-// import React from "react";
-// import "./Card.css";
-
-// //this component is only rendering the images to the screen and can be a simple render function it is a
-// //stateless component
-// const Card = props => (
-//   <div className="card img-container hover">
-//       <img alt={props.name} src={props.image} id={props.id}
-//         onClick={() => props.shuffleScoreCard(props.id)} className='shuffleScore'/>
-//   </div>
-// );
-
-// export default Card;
