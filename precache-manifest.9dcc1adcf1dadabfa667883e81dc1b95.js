a4822ca9a58b1962bd7cb74b3a76a71b8091bdea
Preeti Gupta <preetigupta510@gmail.com>
2020-08-11 19:08:40 -0700

    Updates

---
+++ precache-manifest.9dcc1adcf1dadabfa667883e81dc1b95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c79655fb791b1fc685628e6ee4239ad",
+    "url": "/click-game/index.html"
+  },
+  {
+    "revision": "0c67893a4f747278d9b6",
+    "url": "/click-game/static/css/main.6077920b.chunk.css"
+  },
+  {
+    "revision": "12c1b7031a7fd0a76aee",
+    "url": "/click-game/static/js/2.d0121ab0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/click-game/static/js/2.d0121ab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c67893a4f747278d9b6",
+    "url": "/click-game/static/js/main.ff6d809f.chunk.js"
+  },
+  {
+    "revision": "53021ac0b94d203cb58d",
+    "url": "/click-game/static/js/runtime-main.46fbd14d.js"
+  }
+]);
