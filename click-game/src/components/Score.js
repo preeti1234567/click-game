a6647c44d2e1107b420f32b28311be2d1393bcc0
Preeti Gupta <preetigupta510@gmail.com>
2020-08-11 18:59:27 -0700

    updating ignore

--- click-game/src/components/Score.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Score = props => (
-  <div className="gameScore">
-    <h3 className="score   m-3 text-right">Your Score {props.score} : Top Score {props.topScore} </h3>    
-  </div>
-);
-
-export default Score;
